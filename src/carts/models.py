342f5aa5ccc16da88cee33ef4464d7071396c3d0
Jose-Oquendo <oquendojuanjose@gmail.com>
2022-03-02 19:02:47 -0500

    se ha creado un id unico a cada carrito por usuario

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,11 +1,15 @@
+from enum import unique
 from itertools import product
+from signal import signal
 from django.db import models
+from django.db.models.signals import pre_save
 # Create your models here.
-
+import uuid
 from users.models import User
 from products.models import Product
 
 class Cart(models.Model):
+    cart_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     subtotal = models.DecimalField(default= 0.0, max_digits= 8, decimal_places=2)
@@ -14,3 +18,9 @@ class Cart(models.Model):
 
     def __str__(self):
         return '' 
+
+def ser_cart_id(sender, instance, *args, **kwargs):
+    if not instance.cart_id:
+        instance.cart_id = str(uuid.uuid4())
+
+pre_save.connect(ser_cart_id, sender=Cart)
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from carts.models import Cart
 # Create your views here.
 
+from carts.models import Cart
 def cart(request):
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+    if cart_id:
+        cart = Cart.objects.get(pk=cart_id)
+    else:
+        cart = Cart.objects.create(user=user)
+    request.session['cart_id'] = cart.id
     return render(request, 'carts.html', {})
