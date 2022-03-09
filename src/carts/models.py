4d41db09c190804f7fd142a7b51be9e92237fc47
Jose-Oquendo <oquendojuanjose@gmail.com>
2022-03-09 17:32:07 -0500

    Se ha creado enlaces para hacer mas facil el proceso de añadir al carrito. En el carrito creamos una funcion para sumar el total de la compra

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,12 +1,12 @@
-from enum import unique
+import decimal
 from itertools import product
-from signal import signal
+import uuid
 from django.db import models
 from django.db.models.signals import pre_save
-# Create your models here.
-import uuid
+from django.db.models.signals import m2m_changed
 from users.models import User
 from products.models import Product
+
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
@@ -16,11 +16,36 @@ class Cart(models.Model):
     total = models.DecimalField(default= 0.0, max_digits= 8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    FEE = 0.05
+
     def __str__(self):
         return self.cart_id
 
-def ser_cart_id(sender, instance, *args, **kwargs):
+    def update_totals(self):
+        self.update_subtotals()
+        self.update_total()
+
+    def update_subtotals(self):
+        self.subtotal = sum([product.price for product in self.products.all()])
+        self.save()
+
+    def update_total(self):
+        self.total = self.subtotal + (self.subtotal * (decimal.Decimal(Cart.FEE)))
+        self.save()
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
         instance.cart_id = str(uuid.uuid4())
 
-pre_save.connect(ser_cart_id, sender=Cart)
+def update_totals(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.Cart.update_totals()
+
+pre_save.connect(set_cart_id, sender=Cart)
+m2m_changed.connect(update_totals,sender=Cart)
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,7 +1,9 @@
 from asyncio import ProactorEventLoop
 from itertools import product
 from math import prod
-from django.shortcuts import render
+from this import d
+from webbrowser import get
+from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import Cart
 # Create your views here.
 
@@ -11,17 +13,22 @@ from products.models import Product
 
 def cart(request):
     cart = get_or_create_cart(request)
+    cart2 = cart.products.all()
     return render(request, 'carts/carts.html', {'cart':cart}) #ojo
 
 
 def add(request):
     cart = get_or_create_cart(request)
-    print('Hola este es el error ', request.POST.get('product_id'))
-    num_id = request.POST.get('product_id')
-    product2 = Product.objects.get(pk=num_id)
+    product2 = Product.objects.get(pk=request.POST.get('product_id'))
+    prod = get_object_or_404(Product, pk=request.POST.get('product_id'))
     cart.products.add(product2)
-    print('este es el producto ', product2)
-
     return render(request, 'carts/add.html', {
         'product': product2
     })
+
+def remove(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+    prod = get_object_or_404(Product, pk=request.POST.get('product_id'))
+    cart.products.remove(product)
+    return redirect('carts:carts')
